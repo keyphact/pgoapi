c9406fa391e2e07518d7fd0a9255349edb22823c
MerlionRock <merlionrock@yahoo.com.sg>
2017-02-10 16:38:36 +0800

    Update ver_information.py

--- pgoapi/ver_information.py
+++ pgoapi/ver_information.py
@@ -1,9 +1,5 @@
 class VersionInformation(object):
     ENDPOINT = 'https://pokehash.buddyauth.com/api/v125/hash'
-    POGOAPI_VERSION_DEFAULT = 4500
-    POGOAPI_VERSION_LATEST = 5500
-    
-    def get_str_version(self, pogoapi_ver):
-        tmp_version = str(pogoapi_ver)
-        str_version = '0_' + tmp_version[0:2]
-        return str_version
+    POGOAPI_VERSION_DEFAULT = '0_45'
+    POGOAPI_VERSION_LATEST = '0_55'
+    POGOAPI_VERSION = 5500
